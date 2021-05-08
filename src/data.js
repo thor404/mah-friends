79c23d9ba47d17e0d3d4838fef2142f76e17856d
ehalt <torikussadikk@gmail.com>
2021-05-08 18:02:53 +0600

    fnd

---
+++ src/data.js
@@ -0,0 +1,168 @@
+export default [
+    {
+        id: 1,
+        name: 'Abir',
+        age: 21,
+        img : 'https://i.ibb.co/XxNZkSq/abir.png',
+        scl : 'RABN'
+    },
+    {
+        id: 2,
+        name: 'Shakil Babu',
+        age: 20,
+        img:
+          'https://i.ibb.co/hXTqFKh/babu.jpg',
+        scl: 'VT'
+      },
+    {
+        id: 3,
+        name: 'Rakibul Isalm Rakib',
+        age: 21,
+        img : 'https://i.ibb.co/TkyMV8m/rakibul.png',
+        scl : 'RABN'
+    },
+    {
+        id: 4,
+        name: 'Mahabub E Elahi Rahat',
+        age: 21,
+        img : 'https://i.ibb.co/5FVS35k/rahat.png',
+        scl : 'RABN'
+    },
+    {
+        id: 5,
+        name: 'Abu Talha',
+        age: 21,
+        img : 'https://i.ibb.co/nrBMGTZ/talha.png',
+        scl : 'HS'
+    },
+    {
+        id: 6,
+        name: 'Najmul Huda Mahim',
+        age: 21,
+        img : 'https://i.ibb.co/G0jb0Hn/huda.png',
+        scl : 'HS'
+    },
+    {
+        id: 7,
+        name: 'Touhid Ar Rahman',
+        age: 21,
+        img : 'https://i.ibb.co/64vFncL/touhid.png',
+        scl : 'NPS'
+    },
+    {
+        id: 8,
+        name: 'Rashedul Alam Tomal',
+        age: 21,
+        img : 'https://i.ibb.co/XZ6Jv0D/tomal.png',
+        scl : 'RABN'
+    },
+    {
+        id: 9,
+        name: 'Sonjoy',
+        age: 21,
+        img : 'https://i.ibb.co/9GZn712/sonjoy.png',
+        scl : 'RABN'
+    },
+    {
+        id: 10,
+        name: 'Sohanul Alam',
+        age: 21,
+        img : 'https://i.ibb.co/R9N1sZJ/sohanul.png',
+        scl : 'RABN'
+    },
+    {
+        id: 11,
+        name: 'Mehedi Hasan Siju',
+        age: 21,
+        img : 'https://i.ibb.co/BKsqcMx/siju.png',
+        scl : 'RABN'
+    },
+    {
+        id: 12,
+        name: 'Rishad Hasan',
+        age: 21,
+        img : 'https://i.ibb.co/5GmwXFd/rishad.png',
+        scl : 'RABN'
+    },
+    {
+        id: 13,
+        name: 'Maruf Hasan',
+        age: 21,
+        img : 'https://i.ibb.co/2N0Kmnr/maruf.png',
+        scl : 'RABN'
+    },
+    {
+        id: 14,
+        name: 'Estiak Kapol',
+        age: 21,
+        img : 'https://i.ibb.co/7kR8RJt/kapol.png',
+        scl : 'RABN'
+    },
+    {
+        id: 15,
+        name: 'Kushal Benargy Kabyo',
+        age: 21,
+        img : 'https://i.ibb.co/2sNMCLC/kabyo.png',
+        scl : 'RABN'
+    },
+    {
+        id: 16,
+        name: 'Ahosan Habib Alif',
+        age: 21,
+        img : 'https://i.ibb.co/ZHLRkJ8/alif.png',
+        scl : 'RABN'
+    },
+    {
+        id: 17,
+        name: 'AHM Fahim Morshed',
+        age: 21,
+        img : 'https://i.ibb.co/bsN6Vxz/ahm-fahim.jpg',
+        scl : 'VT'
+    },
+    {
+        id: 16,
+        name: 'Abdul Halim Khan',
+        age: 21,
+        img : 'https://i.ibb.co/4mCb7z6/Ahalim.png',
+        scl : 'VT'
+    },
+    {
+        id: 19,
+        name: 'Arafat Hossain',
+        age: 21,
+        img : 'https://i.ibb.co/hZ5fVfY/arafat.png',
+        scl : 'VT'
+    },
+    {
+        id: 20,
+        name: 'Shakil Haque',
+        age: 21,
+        img : 'https://i.ibb.co/YZ5dKDP/munsi.png',
+        scl : 'VT'
+    },
+    {
+        id: 21,
+        name: 'Siam',
+        age: 21,
+        img : 'https://i.ibb.co/VpK12rH/siam.png',
+        scl : 'VT'
+    },
+    {
+        id: 22,
+        name: 'Fahim Foysal Toha',
+        age: 21,
+        img : 'https://i.ibb.co/nRWLnZD/foysal.jpg',
+        scl : 'VT'
+    },
+    {
+        id: 23,
+        name: 'Sadik Sammo',
+        age: 21,
+        img: 'https://i.ibb.co/r2XvVmT/sammo.png',
+        scl: 'GC'
+    }
+
+
+
+
+  ];
